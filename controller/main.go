fab780766ccf57746228ffad18491bce6821663f
George Dunlap <george.dunlap@citrix.com>
2016-05-27 10:55:37 +0100

    Controller: Move process worker into its own file
    Also fix Makefile rule to use correct automatic variable.
    
    Signed-off-by: George Dunlap <george.dunlap@citrix.com>

--- controller/main.go
+++ controller/main.go
@@ -3,11 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/exec"
 	"os/signal"
-	"encoding/json"
-	"bufio"
-	"io"
 	
 )
 
@@ -23,61 +19,6 @@ type Worker interface {
 	Init() error
 	Shutdown()
 	Process(chan WorkerReport, chan bool)
-}
-
-type ProcessWorker struct {
-	id int
-	c *exec.Cmd
-	stdout io.ReadCloser
-	jsonStarted bool
-}
-
-func (w *ProcessWorker) SetId(i int) {
-	w.id = i
-}
-
-func (w *ProcessWorker) Init() (err error) {
-	w.c = exec.Command("../worker/worker-proc", "burnwait", "20", "20000000")
-
-	w.stdout, err = w.c.StdoutPipe()
-	if err != nil {
-		fmt.Print("Conneting to stdout: ", err)
-		return
-	}
-
-	return
-}
-
-func (w *ProcessWorker) Shutdown() {
-	w.c.Process.Kill()
-}
-
-func (w *ProcessWorker) Process(report chan WorkerReport, done chan bool) {
-	w.c.Start()
-
-	scanner := bufio.NewScanner(w.stdout)
-
-	for scanner.Scan() {
-		s := scanner.Text()
-		
-		//fmt.Println("Got these bytes: ", s);
-
-		if w.jsonStarted {
-			var r WorkerReport
-			json.Unmarshal([]byte(s), &r)
-			r.Id = w.id
-			report <- r
-		} else {
-			if s == "START JSON" {
-				//fmt.Println("Got token to start parsing json")
-				w.jsonStarted = true
-			}
-		}
-	}
-
-	done <- true
-
-	w.c.Wait()
 }
 
 const (
---
+++ controller/processworker.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"encoding/json"
+	"bufio"
+	"io"
+	
+)
+
+type ProcessWorker struct {
+	id int
+	c *exec.Cmd
+	stdout io.ReadCloser
+	jsonStarted bool
+}
+
+func (w *ProcessWorker) SetId(i int) {
+	w.id = i
+}
+
+func (w *ProcessWorker) Init() (err error) {
+	w.c = exec.Command("../worker/worker-proc", "burnwait", "20", "20000000")
+
+	w.stdout, err = w.c.StdoutPipe()
+	if err != nil {
+		fmt.Print("Conneting to stdout: ", err)
+		return
+	}
+
+	return
+}
+
+func (w *ProcessWorker) Shutdown() {
+	w.c.Process.Kill()
+}
+
+func (w *ProcessWorker) Process(report chan WorkerReport, done chan bool) {
+	w.c.Start()
+
+	scanner := bufio.NewScanner(w.stdout)
+
+	for scanner.Scan() {
+		s := scanner.Text()
+		
+		//fmt.Println("Got these bytes: ", s);
+
+		if w.jsonStarted {
+			var r WorkerReport
+			json.Unmarshal([]byte(s), &r)
+			r.Id = w.id
+			report <- r
+		} else {
+			if s == "START JSON" {
+				//fmt.Println("Got token to start parsing json")
+				w.jsonStarted = true
+			}
+		}
+	}
+
+	done <- true
+
+	w.c.Wait()
+}
+
